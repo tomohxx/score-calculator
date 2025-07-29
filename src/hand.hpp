3770a1aa7354b3ebebef05fe4d517a970bf0afe5
tomohxx <43703429+tomohxx@users.noreply.github.com>
2025-07-29 22:22:56 +0900

    Add cast operator

--- src/hand.hpp
+++ src/hand.hpp
@@ -16,6 +16,7 @@ namespace score_calculator {
         : tiles{tiles}, red_dora{red_dora} {}
     explicit Hand(const Tiles& tiles) { draw(tiles); }
     explicit Hand(const std::vector<int>& tiles) { std::copy(tiles.begin(), tiles.end(), this->tiles.begin()); }
+    explicit operator std::vector<int>() { return std::vector<int>(tiles.begin(), tiles.end()); }
 
     Hand& draw(const Tile& tile);
     Hand& discard(const Tile& tile);
