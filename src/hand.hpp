6f73cee49bd13d8db0e8090d635f7721e161a0b3
tomohxx <43703429+tomohxx@users.noreply.github.com>
2026-02-08 19:45:45 +0900

    Apply deducing this

--- src/hand.hpp
+++ src/hand.hpp
@@ -5,6 +5,8 @@
 #include "types.hpp"
 #include <algorithm>
 #include <array>
+#include <cassert>
+#include <utility>
 
 namespace score_calculator {
   struct Hand {
@@ -14,16 +16,45 @@ namespace score_calculator {
     Hand() : tiles{{}}, red_dora{{}} {}
     Hand(const Arr& tiles, const Arr& red_dora)
         : tiles{tiles}, red_dora{red_dora} {}
-    explicit Hand(const Tiles& tiles) { draw(tiles); }
     explicit Hand(const std::vector<int>& tiles) { std::copy(tiles.begin(), tiles.end(), this->tiles.begin()); }
     explicit operator std::vector<int>() { return std::vector<int>(tiles.begin(), tiles.end()); }
-
-    Hand& draw(const Tile& tile);
-    Hand& discard(const Tile& tile);
-    Hand& draw(const Tiles& tiles);
-    Hand& discard(const Tiles& tiles);
-
     bool operator==(const Hand& rhs) const { return tiles == rhs.tiles && red_dora == rhs.red_dora; }
+
+    auto draw(this auto&& self, const Tile& tile) -> decltype(auto)
+    {
+      assert(self.tiles[tile.index] < 4);
+
+      ++self.tiles[tile.index];
+      if (tile.is_red) ++self.red_dora[tile.index];
+
+      return std::forward<decltype(self)>(self);
+    }
+
+    auto discard(this auto&& self, const Tile& tile) -> decltype(auto)
+    {
+      assert(self.tiles[tile.index] > 0);
+
+      --self.tiles[tile.index];
+      if (tile.is_red) --self.red_dora[tile.index];
+
+      return std::forward<decltype(self)>(self);
+    }
+
+    auto draw(this auto&& self, const Tiles& tiles) -> decltype(auto)
+    {
+      std::for_each(tiles.cbegin(), tiles.cend(), [&self](const Tile& tile) { self.draw(tile); });
+
+      return std::forward<decltype(self)>(self);
+    }
+
+    auto discard(this auto&& self, const Tiles& tiles) -> decltype(auto)
+    {
+      std::for_each(tiles.cbegin(), tiles.cend(), [&self](const Tile& tile) { self.discard(tile); });
+
+      return std::forward<decltype(self)>(self);
+    }
+
+    explicit Hand(const Tiles& tiles) { draw(tiles); }
   };
 }
 
